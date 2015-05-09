b0ba9cd1e628d8db00730e62f1f62772bf60ab8c
choang91 <choang@wisc.edu>
2015-05-09 18:53:14 -0500

    adding messaging

---
+++ app/src/main/java/com/DPAC/collabormate/main/Consts.java
@@ -0,0 +1,24 @@
+package com.DPAC.collabormate.main;
+
+public class Consts {
+
+    // QuickBlox credentials
+    public static final int APP_ID = 23015;
+    public static final String AUTH_KEY = "BAwqGjLaRT7gBKH";
+    public static final String AUTH_SECRET = "nSCTPqYpQw9DMRV";
+    public static final String USER_LOGIN = "choang";
+    public static final String USER_PASSWORD = "huuchi12";
+
+    // In GCM, the Sender ID is a project ID that you acquire from the API console
+    public static final String PROJECT_NUMBER = "761750217637";
+
+    public static final String EXTRA_MESSAGE = "message";
+
+    public static final String GCM_NOTIFICATION = "GCM Notification";
+    public static final String GCM_DELETED_MESSAGE = "Deleted messages on server: ";
+    public static final String GCM_INTENT_SERVICE = "GcmIntentService";
+    public static final String GCM_SEND_ERROR = "Send error: ";
+    public static final String GCM_RECEIVED = "Received: ";
+
+    public static final String NEW_PUSH_EVENT = "new-push-event";
+}
